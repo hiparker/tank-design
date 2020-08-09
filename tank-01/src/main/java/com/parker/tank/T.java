d96b076bd6d5f0c9b1b2c0476b4df47b5cbeb960
Parker <meet.parker@foxmail.com>
2020-08-10 01:44:16 +0800

    继承 Frame类 实现 paint

---
+++ tank-01/src/main/java/com/parker/tank/T.java
@@ -0,0 +1,17 @@
+package com.parker.tank;
+
+/**
+ * @BelongsProject: tank-01
+ * @BelongsPackage: com.parker.tank
+ * @Author: Parker
+ * @CreateTime: 2020-08-10 01:32
+ * @Description: 坦克 主入口
+ */
+public class T {
+
+
+    public static void main(String[] args) {
+        TankFrame t = new TankFrame();
+    }
+
+}
---
+++ tank-01/src/main/java/com/parker/tank/TankFrame.java
@@ -0,0 +1,43 @@
+package com.parker.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @BelongsProject: tank-01
+ * @BelongsPackage: com.parker.tank
+ * @Author: Parker
+ * @CreateTime: 2020-08-10 01:14
+ * @Description: 坦克 认识 Frame
+ */
+public class TankFrame extends Frame{
+
+    private static final String TITLE = "坦克大战 v1.0.0";
+
+    public TankFrame(){
+        // 可见
+        this.setVisible(true);
+        // 设置窗口大小
+        this.setSize(800,600);
+        // 禁止改变大小
+        this.setResizable(false);
+        // 设置标题
+        this.setTitle(TITLE);
+
+        // window 监听器
+        this.addWindowListener(new WindowAdapter() {
+
+            // 监听 关闭事件 关闭当前java程序
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.fillRect(200,200,50,50);
+    }
+}
